ce129bee54a6a38ba8c6cc7f1fa060244901410d
devilesk <devilesk@gmail.com>
2016-12-28 20:46:54 -0500

    update openlayers build

--- scripts/build_ol.js
+++ scripts/build_ol.js
@@ -4,4 +4,4 @@ var dir = path.resolve(process.cwd(), './ol2/build');
 console.log(dir);
 var compiler = process.argv[3] || '';
 
-spawn('python', ['build.py', '-c', compiler, '../../interactivemap.cfg'], { cwd: dir, stdio: 'inherit' });
+spawn('python', ['build.py', '-c', compiler, '../../interactivemap.cfg', '../../src/OpenLayers.js'], { cwd: dir, stdio: 'inherit' });
