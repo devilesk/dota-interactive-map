07aba809234dcb5944cd7249f2a2ff5aaaa5d002
devilesk <devilesk@gmail.com>
2016-12-28 20:21:15 -0500

    update openlayers build task

--- scripts/build_ol.js
+++ scripts/build_ol.js
@@ -2,17 +2,6 @@ var path = require('path');
 var spawn = require('child_process').spawn;
 var dir = path.resolve(process.cwd(), './ol2/build');
 console.log(dir);
+var compiler = process.argv[3] || '';
 
-/*http://docs.openlayers.org/library/deploying.html
-
-The options available for compression are:
-
-closure
-
-This requires you to have a closure-compiler.jar in your tools directory. You can do this by fetching the compiler from:
-
-http://closure-compiler.googlecode.com/files/compiler-latest.zip
-
-Then unzipping that file, and placing compiler.jar into tools and renaming it closure-compiler.jar.*/
-
-spawn('python', ['build.py', '-c', 'closure', '../../interactivemap.cfg'], { cwd: dir, stdio: 'inherit' });
+spawn('python', ['build.py', '-c', compiler, '../../interactivemap.cfg'], { cwd: dir, stdio: 'inherit' });
