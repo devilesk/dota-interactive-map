946541a0dc299ff3cfa537f45c2b61a2acee4725
devilesk <devilesk@gmail.com>
2019-05-01 11:46:22 +0000

    refactor: use named exports

--- src/js/conversion.js
+++ src/js/conversion.js
@@ -1,26 +1,26 @@
 import mapConstants from './mapConstants';
 
-const lerp = (minVal, maxVal, pos_r) => pos_r * (maxVal - minVal) + minVal;
+export const lerp = (minVal, maxVal, pos_r) => pos_r * (maxVal - minVal) + minVal;
 
-const reverseLerp = (minVal, maxVal, pos) => (pos - minVal) / (maxVal - minVal);
+export const reverseLerp = (minVal, maxVal, pos) => (pos - minVal) / (maxVal - minVal);
 
-const latLonToWorld = coordinate => {
+export const latLonToWorld = coordinate => {
     const x_r = lerp(mapConstants.map_x_boundaries[0], mapConstants.map_x_boundaries[1], coordinate[0] / mapConstants.map_w),
         y_r = lerp(mapConstants.map_y_boundaries[0], mapConstants.map_y_boundaries[1], (mapConstants.map_h - coordinate[1]) / mapConstants.map_h);
     return [x_r, y_r];
 }
 
-const worldToLatLon = coordinate => {
+export const worldToLatLon = coordinate => {
     const x = reverseLerp(mapConstants.map_x_boundaries[0], mapConstants.map_x_boundaries[1], coordinate[0]) * mapConstants.map_w,
         y = mapConstants.map_h - reverseLerp(mapConstants.map_y_boundaries[0], mapConstants.map_y_boundaries[1], coordinate[1]) * mapConstants.map_h;
     return [x, y];
 }
 
-const getTileRadius = r => parseInt(Math.floor(r / 64));
+export const getTileRadius = r => parseInt(Math.floor(r / 64));
 
-const getScaledRadius = r => r / (mapConstants.map_x_boundaries[1] - mapConstants.map_x_boundaries[0]) * mapConstants.map_w;
+export const getScaledRadius = r => r / (mapConstants.map_x_boundaries[1] - mapConstants.map_x_boundaries[0]) * mapConstants.map_w;
 
-const calculateDistance = (order, units, measure) => {
+export const calculateDistance = (order, units, measure) => {
     if (order == 1) {
         if (units == "km") {
             return measure * mapConstants.scale * 1000;
@@ -28,14 +28,4 @@ const calculateDistance = (order, units, measure) => {
         return measure * mapConstants.scale;
     }
     return measure * mapConstants.scale;
-}
-
-export {
-    lerp,
-    reverseLerp,
-    latLonToWorld,
-    worldToLatLon,
-    getTileRadius,
-    getScaledRadius,
-    calculateDistance
 }
