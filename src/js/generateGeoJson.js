8c25585d0d490503e6a93d12b03305887925c02e
devilesk <devilesk@gmail.com>
2019-05-05 17:28:12 +0000

    refactor: add elevation generation

--- src/js/generateGeoJson.js
+++ src/js/generateGeoJson.js
@@ -4,17 +4,66 @@ import union from '@turf/union';
 import { worldToLatLon } from './InteractiveMap/conversion';
 import fs from 'fs';
 
+if (process.argv.length !== 3 && process.argv.length !== 4) throw new Error('Invalid number of arguments. Usage: <DOTA_PATCH_ID> (INDEX)');
+const DOTA_PATCH_ID = process.argv[2];
+const INDEX = parseInt(process.argv[3]) || -1;
+
+console.log(`Generating no_wards.json, ent_fow_blocker_node.json, and elevation.json for patch: ${DOTA_PATCH_ID}`);
+
 const vs = new VisionSimulation(worlddata);
-vs.initialize('assets/img/map_data_720.png', () => {
-    let t1 = Date.now();
-    generateGeoJSON(vs, [vs.gridnav, vs.tools_no_wards], 'no_wards.json');
-    console.log('no_wards.json', `${Date.now() - t1}ms`);
-    t1 = Date.now();
-    generateGeoJSON(vs, [vs.ent_fow_blocker_node], 'ent_fow_blocker_node.json');
-    console.log('ent_fow_blocker_node.json', `${Date.now() - t1}ms`);
+vs.initialize(`assets/img/map_data_${DOTA_PATCH_ID}.png`, () => {
+    let t1;
+    
+    if (INDEX === 0 || INDEX === -1) {
+        t1 = Date.now();
+        console.log('generating no_wards.json');
+        const result = generateGeoJSON(vs, [vs.gridnav, vs.tools_no_wards]);
+        fs.writeFileSync(`assets/data/${DOTA_PATCH_ID}/no_wards.json`, JSON.stringify(result), 'utf8');
+        console.log('no_wards.json', `${Date.now() - t1}ms`);
+    }
+    else {
+        console.log('skipping no_wards.json');
+    }
+    
+    if (INDEX === 1 || INDEX === -1) {
+        t1 = Date.now();
+        console.log('ent_fow_blocker_node no_wards.json');
+        const result = generateGeoJSON(vs, [vs.ent_fow_blocker_node]);
+        fs.writeFileSync(`assets/data/${DOTA_PATCH_ID}/ent_fow_blocker_node.json`, JSON.stringify(result), 'utf8');
+        console.log('ent_fow_blocker_node.json', `${Date.now() - t1}ms`);
+    }
+    else {
+        console.log('skipping ent_fow_blocker_node.json');
+    }
+    
+    if (INDEX === 2 || INDEX === -1) {
+        const elevations = {};
+        for (const [key, pt] of Object.entries(vs.elevationGrid)) {
+            elevations[pt.z] = elevations[pt.z] || {};
+            elevations[pt.z][key] = pt;
+        }
+        console.log('elevations', Object.keys(elevations));
+        const resultCombined = {
+            type: "FeatureCollection",
+            features: [],
+        };
+        for (const [z, grid] of Object.entries(elevations)) {
+            t1 = Date.now();
+            console.log(`generating elevation_${z}.json`);
+            const result = generateGeoJSON(vs, [grid], `assets/data/${DOTA_PATCH_ID}/elevation_${z}.json`);
+            result.properties = { elevation: z };
+            //fs.writeFileSync(`assets/data/${DOTA_PATCH_ID}/elevation_${z}.json`, JSON.stringify(result), 'utf8');
+            resultCombined.features.push(result);
+            console.log(`elevation_${z}.json`, `${Date.now() - t1}ms`);
+        }
+        fs.writeFileSync(`assets/data/${DOTA_PATCH_ID}/elevation.json`, JSON.stringify(resultCombined), 'utf8');
+    }
+    else {
+        console.log('skipping elevation.json');
+    }
 });
 
-const generateGeoJSON = (vs, grids, dst) => {
+const generateGeoJSON = (vs, grids) => {
     let result = null;
     grids.forEach((data, index) => {
         const total = Object.keys(data).length;
@@ -48,8 +97,7 @@ const generateGeoJSON = (vs, grids, dst) => {
         }
     });
     // transformData(result);
-
-    fs.writeFile(dst, JSON.stringify(result), (err) => {});
+    return result;
 };
 
 const transformData = (data) => {
