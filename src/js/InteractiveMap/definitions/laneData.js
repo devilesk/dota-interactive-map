1336dd322b8e51f4caadff67d80da910cc15c514
devilesk <devilesk@gmail.com>
2019-05-05 19:55:33 +0000

    refactor: move laneData to separate file

---
+++ src/js/InteractiveMap/definitions/laneData.js
@@ -0,0 +1,52 @@
+const laneData = {
+    700: {
+        npc_dota_spawner_good_bot: [1.25, 10],
+        npc_dota_spawner_bad_bot: [0.75, 22],
+        npc_dota_spawner_good_top: [0.75, 2],
+        npc_dota_spawner_bad_top: [1.25, 2],
+    },
+    706: {
+        npc_dota_spawner_good_bot: [1.3, 16],
+        npc_dota_spawner_bad_bot: [0.65, 22],
+        npc_dota_spawner_good_top: [1.3, 8],
+        npc_dota_spawner_bad_top: [0.65, 8],
+    },
+    707: {
+        npc_dota_spawner_good_bot: [1.3, 4],
+        npc_dota_spawner_bad_bot: [0.65, 6],
+        npc_dota_spawner_good_top: [1.3, 2],
+        npc_dota_spawner_bad_top: [0.65, 2],
+    },
+    709: {
+        npc_dota_spawner_good_bot: [1.3, 4],
+        npc_dota_spawner_bad_bot: [0.65, 6],
+        npc_dota_spawner_good_top: [1.3, 2],
+        npc_dota_spawner_bad_top: [0.65, 2],
+    },
+    715: {
+        npc_dota_spawner_good_bot: [1.3, 4],
+        npc_dota_spawner_bad_bot: [0.65, 6],
+        npc_dota_spawner_good_top: [1.3, 2],
+        npc_dota_spawner_bad_top: [0.65, 2],
+    },
+    719: {
+        npc_dota_spawner_good_bot: [1.3, 4],
+        npc_dota_spawner_bad_bot: [0.65, 6],
+        npc_dota_spawner_good_top: [1.3, 2],
+        npc_dota_spawner_bad_top: [0.65, 2],
+    },
+    720: {
+        npc_dota_spawner_good_bot: [0, 0],
+        npc_dota_spawner_bad_bot: [0, 0],
+        npc_dota_spawner_good_top: [0, 0],
+        npc_dota_spawner_bad_top: [0, 0],
+    },
+    721: {
+        npc_dota_spawner_good_bot: [0, 0],
+        npc_dota_spawner_bad_bot: [0, 0],
+        npc_dota_spawner_good_top: [0, 0],
+        npc_dota_spawner_bad_top: [0, 0],
+    },
+};
+
+export default laneData;
