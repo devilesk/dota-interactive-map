921c003f0aeefb49886ca90c95ed70e2e6a0d98f
devilesk <devilesk@gmail.com>
2019-11-25 14:57:20 +0000

    fix: update css script

--- scripts/css.js
+++ scripts/css.js
@@ -12,16 +12,16 @@ sass.render({ file: 'src/scss/app.scss' }, (err, renderedCss) => {
         console.log('Css compiled.');
         let contents = renderedCss.css;
         let outFile = 'build/app.css';
-        fs.writeFile(outFile, contents, (err) => {
+        fs.writeFile(outFile, contents, 'utf8', (err) => {
             if (err) {
                 console.log(err);
             }
             else {
                 console.log(`${outFile} was successfully saved.`);
                 if (process.env.NODE_ENV === 'production') {
-                    contents = new CleanCSS({ rebase: false }).minify(outFile);
+                    const result = new CleanCSS({ rebase: false }).minify(contents).styles;
                     outFile = `build/app-${git.short()}.min.css`;
-                    fs.writeFile(outFile, contents, (err) => {
+                    fs.writeFile(outFile, result, (err) => {
                         if (err) {
                             console.log(err);
                         }
